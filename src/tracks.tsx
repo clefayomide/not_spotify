c571bcf6e4b7a4c67b76e0590380ce40eab635f2
victor olorunfemi <victorayomide32@gmail.com>
2023-04-16 07:36:32 +0100

    track change

--- src/tracks.tsx
+++ src/tracks.tsx
@@ -1,19 +1,19 @@
-import solace from "./assets/solace_cover.jpg";
-import loyalty from "./assets/loyalty.jpeg";
+import crayon from "./assets/crayon.webp";
+import jt from "./assets/jt.jpg";
 
 export const tracks = [
   {
-    title: "Loyalty",
-    artist: "Ajebo hustlers",
-    cover_image: loyalty,
+    title: "JT",
+    artist: "Jon Bellion",
+    cover_image: jt,
     track_link:
-      "https://res.cloudinary.com/clefayomide/video/upload/v1672741490/rxiivqoxs9waglgszwl0.mp3",
+      "https://res.cloudinary.com/clefayomide/video/upload/v1681625255/is84obqfjcayxb239pii.mp3",
   },
   {
-    title: "Solace",
-    artist: "Ajebo hustlers",
-    cover_image: solace,
+    title: "Ijo labalaba",
+    artist: "Crayon",
+    cover_image: crayon,
     track_link:
-      "https://res.cloudinary.com/clefayomide/video/upload/v1671992305/t3zeijc3kyct71vy04s2.mp3",
+      "https://res.cloudinary.com/clefayomide/video/upload/v1681626185/y6zy0h9jaftr7jcqp4a5.mp3",
   },
 ];
